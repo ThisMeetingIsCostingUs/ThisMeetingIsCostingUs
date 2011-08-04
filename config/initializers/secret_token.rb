ae3686334bfd2616bc6bfd12e96427e184bb460d
Jason Lunn <jlunn@jungroup.com>
2011-08-03 21:47:39 -0400

    Empty rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ThisMeetingIsCostingUs::Application.config.secret_token = '915e1f4a68f8e6decc9f1c127571e754cca21de633ce5d7ff7a6ec76ae852e188e0e17d4b2a7230f9e66fdb432dd1ba27bd0dda24308f3f2a4d55133e4fc0f34'
