b4cd5d76e188957e63a18312ad1849c6f7a0e4bf
Vincent Batts <vbatts@hashbangbash.com>
2011-08-06 22:52:47 -0400

    adding the view of a mostly javascript flipping counter. It will continue to expand as the counter increases, which was the limitation of the clock previously found. Additionally, this counter is *explicitly* licensed under the MIT license.

---
+++ app/controllers/flip_counter_controller.rb
@@ -0,0 +1,5 @@
+class FlipCounterController < ApplicationController
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ThisMeetingIsCostingUs::Application.routes.draw do
+
+  resources :counter
+  resources :flipCounter
+
   get "counter/index"
 
   # The priority is based upon order of creation:
@@ -51,6 +55,8 @@ ThisMeetingIsCostingUs::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => "welcome#index"
+
+  #root :controller => "counter"
 
   # See how all your routes lay out with "rake routes"
 
